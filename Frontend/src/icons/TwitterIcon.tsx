c9a115dbcbd2072e5303d24446c44442a4664a6a
Asheesh Rawat <asheeshrawat48@gmail.com>
2026-07-02 05:39:51 +0530

    add yt and twitter icon

---
+++ Frontend/src/icons/TwitterIcon.tsx
@@ -0,0 +1,36 @@
+function TwitterIcon() {
+  return (
+    <svg
+      id="Twitter--Streamline-Atlas"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="-0.5 -0.5 16 16"
+      className="size-5"
+    >
+      <desc>Twitter Streamline Icon: https://streamlinehq.com</desc>
+      <defs></defs>
+      <path
+        d="M5.3125 10.625c-1.70625 -0.325 -2.70625 -0.9750000000000001 -3.125 -1.875"
+        fill="none"
+        stroke="#000000"
+        stroke-miterlimit="10"
+        stroke-width="1"
+      ></path>
+      <path
+        d="M2.8125 6.25c-0.71875 -1.2 -1.0625 -3.3625 -0.625 -3.75 1.9375 1.5625 3.825 2.55625 5.625 2.5 0 0 0 -3.125 2.5 -3.125a2.7375 2.7375 0 0 1 1.875 0.625h1.875l-0.625 1.875a8.418750000000001 8.418750000000001 0 0 1 -2.5 6.875c-3.125 3.125 -8.125 2.5 -10 0"
+        fill="none"
+        stroke="#000000"
+        stroke-miterlimit="10"
+        stroke-width="1"
+      ></path>
+      <path
+        d="M3.4375 8.75a4.73125 4.73125 0 0 1 -1.875 -3.125"
+        fill="none"
+        stroke="#000000"
+        stroke-miterlimit="10"
+        stroke-width="1"
+      ></path>
+    </svg>
+  );
+}
+
+export default TwitterIcon;
